9f0248498cd35fa12125d0adfba9c63a747856dd
Timandes White <timands@gmail.com>
2014-03-01 22:43:23 +0800

    finish main logic.

--- Timandes/CLI/Service.php
+++ Timandes/CLI/Service.php
@@ -6,10 +6,9 @@ namespace Timandes\CLI;
 
 class Service
 {
-    public function __construct($oCallback, $nMaxSubProcesses = 5)
+    public function __construct($oCallback)
     {
         $this->_callback = $oCallback;
-        $this->_maxSubProcessNum = $nMaxSubProcesses;
         $this->_eventBase = event_base_new();
     }
 
@@ -20,36 +19,71 @@ class Service
     }
 
 
-    public static function create($oCallback, $nMaxSubProcesses = 5)
+    public static function create($oCallback)
     {
-        return new Service($oCallback, $nMaxSubProcesses);
+        return new Service($oCallback);
     }
 
 
-    public function start()
+    public function start($nMaxSubProcesses = 5)
     {
-        for ($i=0; $i<$this->_maxSubProcessNum; ++$i) {
-            $iSubProcessId = pcntl_fork();
-            if ($iSubProcessId < 0) {
-                $this->_killAllWorkerProcesses();
+        // TODO: daemonize
+
+        for ($i=0; $i<$nMaxSubProcesses; ++$i) {
+            $iSubProcessId = $this->_forkChildProcess();
+            if ($iSubProcessId < 0)
                 return 1;
-            }
-
-            if ($iSubProcessId == 0) { // sub process
-                exit(call_user_func($this->_callback));
-            }
-
-            $this->_processMapping[$iSubProcessId] = $iSubProcessId;
-            ++$this->_accProcessNum;
         }
 
-        $this->_registerSignal(SIGTERM);
-        $this->_registerSignal(SIGQUIT);
-        $this->_registerSignal(SIGINT);
+        $this->_hookSignalForMainProcess(SIGTERM);
+        $this->_hookSignalForMainProcess(SIGQUIT);
+        $this->_hookSignalForMainProcess(SIGINT);
+        $this->_hookSignalForMainProcess(SIGCHLD);
 
-        // TODO: add loop here ...
+
+        while (!event_base_loop($this->_eventBase)
+                && !$this->_processExiting
+                && $this->_accProcessNum < $this->_maxTotalProcesses) {
+            $iSubProcessId = $this->_forkChildProcess();
+            if ($iSubProcessId < 0)
+                return 1;
+        }
 
         return 0;
+    }
+
+
+    protected function _startChildProcess()
+    {
+        $this->_unhookSignalForMainProcess(SIGTERM);
+        $this->_unhookSignalForMainProcess(SIGQUIT);
+        $this->_unhookSignalForMainProcess(SIGINT);
+        $this->_unhookSignalForMainProcess(SIGCHLD);
+
+        $this->_hookSignalForChildProcess(SIGTERM);
+        $this->_hookSignalForChildProcess(SIGQUIT);
+        $this->_hookSignalForChildProcess(SIGINT);
+
+        exit(call_user_func($this->_callback));
+    }
+
+
+    protected function _forkChildProcess()
+    {
+        $iSubProcessId = pcntl_fork();
+        if ($iSubProcessId < 0) {
+            $this->_killAllWorkerProcesses();
+            return $iSubProcessId;
+        }
+
+        if ($iSubProcessId == 0) { // sub process
+            $this->_startChildProcess();
+        }
+
+        $this->_processMapping[$iSubProcessId] = $iSubProcessId;
+        ++$this->_accProcessNum;
+
+        return $iSubProcessId;
     }
 
 
@@ -58,18 +92,65 @@ class Service
 
     }
 
-    protected function _registerSignal($iSignal)
+    protected function _handleChildSignal()
+    {
+        $iSubProcessId = pcntl_wait($iWorkerProcessStatus, WNOHANG);
+        if ($iSubProcessId <= 0) {// error or no children quit
+            return;
+        }
+
+        unset($this->_processMapping[$iSubProcessId]);
+
+        event_base_loopexit($this->_eventBase);
+    }
+
+    protected function _handleTerminateSignal() 
+    {
+        if ($this->_processExiting)
+            return;
+        $this->_processExiting = true;
+
+        $this->_killAllWorkerProcesses();
+
+        event_base_loopexit($this->_eventBase);
+    }
+
+    protected function _hookSignalForMainProcess($iSignal)
+    {
+    }
+
+    protected function _unhookSignalForMainProcess($iSignal)
+    {
+
+    }
+
+    protected function _hookSignalForChildProcess($iSignal)
+    {
+    }
+
+    protected function _unhookSignalForChildProcess($iSignal)
     {
 
     }
 
     protected $_callback = null;
-    protected $_maxSubProcessNum = 5;
     protected $_processMapping = array();
     protected $_accProcessNum = 0;
     protected $_eventBase = null;
     protected $_mainSignalHandler = function($iSignal) {
+        switch ($iSignal) {
+            case SIGCHLD:
+                $this->_handleChildSignal();
+                break;
+            case SIGTERM:
+            case SIGQUIT:
+            case SIGINT:
+                $this->_handleTerminateSignal();
+                break;
+        }
     };
     protected $_childSignalHandler = function($iSignal) {
     };
+    protected $_processExiting = false;
+    protected $_maxTotalProcesses = 10000;
 }
