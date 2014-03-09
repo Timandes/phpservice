e347d654ff532e6c3cb1f4f89dabdcf7a24d3c23
Timandes White <timands@gmail.com>
2014-03-09 16:56:01 +0800

    example program.

---
+++ eg.php
@@ -0,0 +1,27 @@
+<?php
+# vim: set ts=4 sts=4 sw=4 expandtab:
+# sublime: tab_size 4; translate_tabs_to_spaces true
+/**
+ * Example: print something
+ *
+ * @package phpservice
+ */
+
+declare(ticks=1);
+
+require 'src/Timandes/CLI/Service.php';
+
+use Timandes\CLI\Service;
+
+$oService = Service::create(function() {
+    $m = mt_rand(1, 100);
+    $s = 0;
+    for ($i = 0; $i < 10; ++$i) {
+        $s += $i * $m;
+
+        fprintf(STDOUT, "Result(#%d)=%d\n", getmygid(), $s);
+        sleep(1);
+    }
+    fprintf(STDOUT, "Finish(#%d)\n", getmygid());
+});
+$oService->start(3);
